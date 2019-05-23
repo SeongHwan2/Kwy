1e1eefb322d9eebe9ec234bbc7cf5263429ad418
GD3 <GD3@DESKTOP-NF6773I>
2019-05-23 15:58:49 +0900

    kwy start

---
+++ src/main/java/kw/yoo/hwan/HomeController.java
@@ -0,0 +1,39 @@
+package kw.yoo.hwan;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+@RequestMapping("/board")
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@RequestMapping("/select")
+	public String select() {
+		logger.info("Welcome home! The client locale is " + 1 + ".");		
+		return "home";
+	}
+	
+	@RequestMapping("/insert")
+	public String insert() {
+		logger.info("Welcome home! The client locale is " + 1 + ".");		
+		return "home";
+	}
+	
+	@RequestMapping("/update")
+	public String update() {
+		logger.info("Welcome home! The client locale is " + 1 + ".");		
+		return "home";
+	}
+	
+	@RequestMapping("/delete")
+	public String delete() {
+		logger.info("Welcome home! The client locale is " + 1 + ".");		
+		return "home";
+	}
+}
