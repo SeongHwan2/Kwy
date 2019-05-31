d65999ce35ef484c949430eadb8cb20c7e3942e8
GD3 <GD3@DESKTOP-NF6773I>
2019-05-31 16:01:36 +0900

    마지막

--- src/main/java/kw/yoo/hwan/HomeController.java
+++ src/main/java/kw/yoo/hwan/HomeController.java
@@ -1,7 +1,12 @@
 package kw.yoo.hwan;
 
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,27 +18,16 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
+	@Autowired
+	SqlSession session;
+	
 	@RequestMapping("/select")
 	public String select() {
-		logger.info("Welcome home! The client locale is " + 1 + ".");		
+		logger.info("Welcome home! The client locale is " + 1 + ".");
+		List<HashMap<String, Object>> resultList = session.selectList("test.select");
+		logger.info("Count : {}", resultList.size());
 		return "home";
 	}
 	
-	@RequestMapping("/insert")
-	public String insert() {
-		logger.info("Welcome home! The client locale is " + 1 + ".");		
-		return "home";
-	}
 	
-	@RequestMapping("/update")
-	public String update() {
-		logger.info("Welcome home! The client locale is " + 1 + ".");		
-		return "home";
-	}
-	
-	@RequestMapping("/delete")
-	public String delete() {
-		logger.info("Welcome home! The client locale is " + 1 + ".");		
-		return "home";
-	}
 }
